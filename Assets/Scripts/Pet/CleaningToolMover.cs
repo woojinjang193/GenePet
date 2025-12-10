4de912ac810a46e65536deb00d6cde17524339eb
장우진 <ryanjang193@gmail.com>
2025-12-11 08:30:35 +0900

    [feat] 씻기기 파티클 추가

--- Assets/Scripts/Pet/CleaningToolMover.cs
+++ Assets/Scripts/Pet/CleaningToolMover.cs
@@ -6,11 +6,18 @@ public class CleaningToolMover : MonoBehaviour
     private float _movedDist; //이동거리 누적
     private PetController _target; //닿아있는 펫
 
+    [SerializeField] private ParticleSystem _bubble;
+    [SerializeField] private ParticleSystem _foam;
+
+    private bool _isBubbling = false;
+    private float _notMovingTimer = 0f;
+
     private void OnEnable()
     {
         _target = null;  //펫 참조 초기화
+        _isBubbling = false;
+        _notMovingTimer = 0f;
     }
-
     private void Update()
     {
         Vector3 cur = transform.position; //현재 위치
@@ -18,12 +25,39 @@ public class CleaningToolMover : MonoBehaviour
         _movedDist += dist; //총 이동거리 누적
         _lastPos = cur; //다음 프레임 비교를 위해 저장
 
-        if (_target != null)  //펫과 닿아있으면
+        bool touching = (_target != null);
+        bool moving = (dist > 0.02f);
+
+        if (touching)  //펫과 닿아있으면
         {
+            if (moving)
+            {
+                _notMovingTimer = 0f;
+
+                if (!_isBubbling)
+                {
+                    _isBubbling = true;
+                    _bubble.Play();
+                    _foam.Play();
+                    Debug.Log("버블 플레이");
+                }
+            }
+            else
+            {
+                _notMovingTimer += Time.deltaTime;
+
+                // 0.5초 이상 멈춘 경우만 Stop
+                if (_notMovingTimer >= 0.5f && _isBubbling)
+                {
+                    _isBubbling = false;
+                    _bubble.Stop();
+                    _foam.Stop();
+                    Debug.Log("버블 멈춤 (0.5초 지속)");
+                }
+            }
             _target.AddCleaningProgress(dist); //펫에게 이동량 전달
         }
     }
-
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Pet"))  //Pet 태그와 닿음
@@ -33,11 +67,13 @@ public class CleaningToolMover : MonoBehaviour
             _movedDist = 0f;  //누적 이동거리 초기화
         }
     }
-
     private void OnTriggerExit2D(Collider2D col)
     {
         if (col.CompareTag("Pet"))  //벗어났으면
         {
+            _bubble.Stop();
+            _foam.Stop();
+            Debug.Log("버블 스탑");
             _target = null;  //연결 해제
         }
     }
