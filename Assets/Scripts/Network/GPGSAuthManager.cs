58c0b4ddf483aa50f032dee41712549d21f8717e
장우진 <ryanjang193@gmail.com>
2026-01-03 20:08:29 +0900

    [fix] GPGS 재설치 후 빌드 성공

--- Assets/Scripts/Network/GPGSAuthManager.cs
+++ Assets/Scripts/Network/GPGSAuthManager.cs
@@ -1,34 +1,34 @@
-//using GooglePlayGames;
+using GooglePlayGames;
 using System;
 using System.Collections;
 using UnityEngine;
 
-//public class GPGSAuthManager : MonoBehaviour
-//{
-//    private void Awake()
-//    {
-//        PlayGamesPlatform.Activate();
-//    }
-//    public void Login(Action<bool> callback) // 로그인 요청 + 결과 콜백
-//    {
-//        // 오프라인이면 즉시 실패
-//        if (Application.internetReachability == NetworkReachability.NotReachable)
-//        {
-//            callback?.Invoke(false); // 실패 반환
-//            return;
-//        }
-//
-//        // 이미 로그인 돼 있으면 성공 처리
-//        if (Social.localUser.authenticated)
-//        {
-//            callback?.Invoke(true);
-//            return;
-//        }
-//
-//        // 로그인 시도
-//        Social.localUser.Authenticate(success =>
-//        {
-//            callback?.Invoke(success); // 결과 전달
-//        });
-//    }
-//}
+public class GPGSAuthManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        PlayGamesPlatform.Activate();
+    }
+    public void Login(Action<bool> callback) // 로그인 요청 + 결과 콜백
+    {
+        // 오프라인이면 즉시 실패
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            callback?.Invoke(false); // 실패 반환
+            return;
+        }
+
+        // 이미 로그인 돼 있으면 성공 처리
+        if (Social.localUser.authenticated)
+        {
+            callback?.Invoke(true);
+            return;
+        }
+
+        // 로그인 시도
+        Social.localUser.Authenticate(success =>
+        {
+            callback?.Invoke(success); // 결과 전달
+        });
+    }
+}
