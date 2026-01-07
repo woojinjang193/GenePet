fa6cc68405bc725ad00e24a9f1fdf3bec2ab266f
장우진 <ryanjang193@gmail.com>
2026-01-08 04:46:15 +0900

    [feat] 레벨 추가

--- Assets/Scripts/MiniGame/ItemForMiniGame.cs
+++ Assets/Scripts/MiniGame/ItemForMiniGame.cs
@@ -1,9 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemForMiniGame : MonoBehaviour
 {
+    [Header("아이템 타입")]
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
     [Header("아이템 타입")]
     [SerializeField] private RewardType _reward;
     [SerializeField] private int _amount;
@@ -11,4 +12,11 @@ public class ItemForMiniGame : MonoBehaviour
     public RewardType Reward => _reward;
     public int Amount => _amount;
     
+    public void Init(Sprite icon, RewardType reward, int amount)
+    {
+        _spriteRenderer.sprite = icon;
+        _reward = reward;
+        _amount = amount;
+        gameObject.SetActive(true);
+    }
 }
