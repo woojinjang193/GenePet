8efe4ac223b751932a3464a5489b6152468302d0
장우진 <ryanjang193@gmail.com>
2026-01-28 18:48:54 +0900

    [test] UI 재설정

---
+++ Assets/Scripts/Pet/PetPetting.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PetPetting : MonoBehaviour
+{
+    private Vector3 _lastPos; //이전 프레임 위치
+    private float _movedDist; //이동거리 누적
+    private PetController _target; //닿아있는 펫
+
+    [SerializeField] private ParticleSystem _heartParticle;
+
+    private bool _isPetting = false;
+    private float _notMovingTimer = 0f;
+
+    private void OnEnable()
+    {
+        _target = null;  //펫 참조 초기화
+        _isPetting = false;
+        _notMovingTimer = 0f;
+    }
+    private void Update()
+    {
+        Vector3 cur = transform.position; //현재 위치
+        float dist = (cur - _lastPos).magnitude;  //프레임 이동거리
+        _movedDist += dist; //총 이동거리 누적
+        _lastPos = cur; //다음 프레임 비교를 위해 저장
+
+        bool touching = (_target != null && !_target.Status.IsLeft);
+        bool moving = (dist > 0.02f);
+
+        if (touching)  //펫과 닿아있으면
+        {
+            if (moving)
+            {
+                _notMovingTimer = 0f;
+
+                if (!_isPetting)
+                {
+                    _isPetting = true;
+                    _heartParticle.Play();
+                    Debug.Log("하트 플레이");
+                }
+            }
+            else
+            {
+                _notMovingTimer += Time.deltaTime;
+
+                // 0.3초 이상 멈춘 경우만 Stop
+                if (_notMovingTimer >= 0.3f && _isPetting)
+                {
+                    _isPetting = false;
+                    _heartParticle.Stop();
+                    Debug.Log("하트 플레이 (0.3초 지속)");
+                }
+            }
+            _target.Clean(dist); //펫에게 이동량 전달
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Pet"))  //Pet 태그와 닿음
+        {
+            _target = col.GetComponent<PetController>();   //펫 컨트롤러 가져오기
+            _lastPos = transform.position; //초기 위치 저장
+            _movedDist = 0f;  //누적 이동거리 초기화
+            _notMovingTimer = 0f; // 파티클 스탑 타이머 초기화
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Pet"))  //벗어났으면
+        {
+            _heartParticle.Stop();
+            _isPetting = false;
+            _notMovingTimer = 0f; // 파티클 스탑 타이머 초기화
+
+            _target = null;  //연결 해제
+            Debug.Log("버블 스탑");
+        }
+    }
+
+}
