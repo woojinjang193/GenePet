c16cea5fb745640c6843b658f0577238371b24f5
장우진 <ryanjang193@gmail.com>
2026-02-02 18:40:17 +0900

    [feat] 리워드 광고 추가

--- Assets/Scripts/Ad/AdManager.cs
+++ Assets/Scripts/Ad/AdManager.cs
@@ -1,4 +1,5 @@
 using GoogleMobileAds.Api;
+using GooglePlayGames.BasicApi;
 using System;
 using System.Collections;
 using UnityEngine;
@@ -19,7 +20,9 @@ public class AdManager : Singleton<AdManager>
     private string _rewardAdID = "ca-app-pub-3940256099942544/1712485313";
 #endif
 
-    BannerView _bannerView;
+    private BannerView _bannerView;
+    private RewardedAd _rewardedAd;
+
     public event Action OnAdRemoved;
     protected override void Awake()
     {
@@ -41,17 +44,18 @@ public class AdManager : Singleton<AdManager>
         {
             if (initstatus == null)
             {
-                Debug.LogError("Google Mobile Ads initialization failed.");
+                Debug.LogError("[AdManager] Google Mobile Ads initialization failed.");
                 return;
             }
-            Debug.Log("Google Mobile Ads initialization complete.");
+            Debug.Log("[AdManager] Google Mobile Ads initialization complete.");
 
             _isMobileAdsInitialized = true;
+            RequestRewarded(); //보상형 광고 미리 로드
             CheckManagerReady();
         });
     }
-
-    public void TryRequestBanner() //광고 띄우기 (씬에서 호출)
+    //===================배너 광고 띄우기(씬에서 호출)===================
+    public void TryRequestBanner() 
     {
         if (!_isMobileAdsInitialized) return; //광고 초기화 아직이면 리턴
         if (IsAdRemoved) return; // //광고제거상태면 리턴
@@ -72,7 +76,77 @@ public class AdManager : Singleton<AdManager>
         _bannerView.LoadAd(adRequest);
         Debug.Log("[AdManager] 배너 로드");
     }
-  
+    //===================리워드 광고 띄우기(씬에서 호출)================
+    public void ShowRewardedAd(RewardType reward) //보상형 광고 띄우기 (외부 호출용)
+    {
+        //const string rewardMsg =
+        //    "Rewarded ad rewarded the user. Type: {0}, amount: {1}.";
+
+        if (_rewardedAd != null && _rewardedAd.CanShowAd()) // 광고 객체가 있고 현재 표시 가능 상태라면
+        {
+            _rewardedAd.Show((Reward reward) => // 광고를 표시하고, 보상 조건 충족 시 콜백 실행
+            {
+                TODO:리워드 지급
+            });
+        }
+    }
+    private void RequestRewarded() //리워드 광고 미리 준비
+    {
+        // Clean up the old ad before loading a new one.
+        if (_rewardedAd != null)
+        {
+            DestroyRewarded(); //이미 있으면 삭제
+        }
+
+        Debug.Log("[AdManager] Loading the rewarded ad.");
+
+        var adRequest = new AdRequest();
+
+        // send the request to load the ad.
+        RewardedAd.Load(_rewardAdID, adRequest,
+            (RewardedAd ad, LoadAdError error) =>
+            {
+                // if error is not null, the load request failed.
+                if (error != null || ad == null)
+                {
+                    return;
+                }
+                _rewardedAd = ad; //광고 등록
+
+                _rewardedAd.OnAdClicked += HandleOnRewardedAdClicked;
+                _rewardedAd.OnAdFullScreenContentClosed += HandleOnRewardedAdClosed;
+                _rewardedAd.OnAdPaid += HandleOnRewardedAdPaid;
+                _rewardedAd.OnAdImpressionRecorded += HandelOnRewardedAdImpression;
+            });
+    }
+
+    public void DestroyRewarded()
+    {
+        if (_rewardedAd != null)
+        {
+            _rewardedAd.Destroy();
+            _rewardedAd = null;
+        }
+    }
+    public void HandleOnRewardedAdClicked() // 리워드 광고 클릭 시 호출되는 핸들러
+    {
+        Debug.Log("[AdManager] 광고 클릭");
+    }
+    public void HandleOnRewardedAdClosed() // 리워드 광고가 닫혔을 때 호출되는 핸들러
+    {
+        Debug.Log("[AdManager] 리워드 광고 닫힘");
+
+        //광고를 보고 클로즈하면 광고를 다시 요청해서 로드
+        RequestRewarded();
+    }
+    public void HandelOnRewardedAdImpression() // 리워드 광고 노출 기록 시 호출되는 핸들러
+    {
+        Debug.Log("[AdManager] Rewarded Ad Impression Recorded");
+    }
+    public void HandleOnRewardedAdPaid(AdValue adValue) // 광고 수익이벤트 발생 시 호출되는 핸들러
+    {
+        Debug.Log("[AdManager] 수익이벤트 발생");
+    }
     //===================광고 제거 관련=====================
     private void CheckRemoveAD()
     {
