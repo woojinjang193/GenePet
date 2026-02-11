3a41356c309dcccde6bffa5328aaf28d7c0b7029
장우진 <ryanjang193@gmail.com>
2026-02-11 16:42:32 +0900

    [fix] 줌인시 다른펫 편지 보이는 문제 수정

--- Assets/Scripts/Game OBJ/Room/RoomSpriteSO.cs
+++ Assets/Scripts/Game OBJ/Room/RoomSpriteSO.cs
@@ -8,7 +8,7 @@ public class RoomSpriteSO : ScriptableObject
     public Sprite Room_Jump;
     public Sprite Room_Rythm;
     public Sprite Room_Pinball;
-    public Sprite Room_Cozy;
+    //public Sprite Room_Cozy;
     public Sprite Room_Poor;
     public Sprite Room_Something;
 
@@ -19,7 +19,7 @@ public class RoomSpriteSO : ScriptableObject
             case Room.Room_Jump: return Room_Jump;
             case Room.Room_Rythm: return Room_Rythm;
             case Room.Room_Pinball: return Room_Pinball;
-            case Room.Room_Cozy: return Room_Cozy;
+            //case Room.Room_Cozy: return Room_Cozy;
             case Room.Room_Poor: return Room_Poor;
             case Room.Room_Something: return Room_Something;
                 //case Room.Room6: return Room1;
